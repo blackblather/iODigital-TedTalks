a2edbc66a8b0910f495e80cff5f1cd71e0f05bd2
blackblather <joao.monteiro95@hotmail.com>
2024-12-03 18:33:18 +0000

    all the locations in the script be relative to the script location

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+this_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$this_path"
+# The code above makes all the locations in the script be relative to the script location (independently of where it's invoked from)
+# SRC: https://stackoverflow.com/questions/24112727/relative-paths-based-on-file-location-instead-of-current-working-directory
+
 # Clean / Compile / Create FAT-Jar file
 ./mvnw clean package
 
